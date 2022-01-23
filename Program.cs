72cd89a4682e0327fe306b7733f5bf7111086503
Marcos Wellington <marcoswoc@gmail.com>
2022-01-23 00:15:26 -0300

    Correções artigo

--- Program.cs
+++ Program.cs
@@ -1,29 +1,62 @@
-﻿// // Trim
-// var  observacao = " Charizard cospe fogo que é quente o suficiente para derreter pedregulhos. ";
-
-// Console.WriteLine($"Observação com espaço: {observacao}");
-// Console.WriteLine($"Observação sem espaço: {observacao.Trim()}");
-
-// // Length
-// var nome = "Charizard";
-
-// Console.WriteLine($"O nome {nome}, tem {nome.Length} caracteres.");
-
-// //ToUpper
-
-// var tipo = "fogo";
-
-// Console.WriteLine($"Charizard é um pokémon do tipo: {tipo.ToUpper()}");
-
-// //ToLower
-// var tipoSecundario = "VOADOR";
-
-// Console.WriteLine($"Charizard é um pokémon do tipo: {tipoSecundario.ToLower()}");
-
-// //Remove
-// Console.WriteLine($"Eu chamo meu {nome} de {nome.Remove(5)}.");
-
-// // Replace
+﻿// Trim
+Console.WriteLine("==== Trim ====");
 var  observacao = " Charizard cospe fogo que é quente o suficiente para derreter pedregulhos. ";
 
+Console.WriteLine($"Observação com espaço: {observacao}");
+Console.WriteLine($"Observação sem espaço: {observacao.Trim()}");
+
+Console.WriteLine("");
+
+// Length
+Console.WriteLine("==== Length ====");
+var nome = "Charizard";
+
+Console.WriteLine($"O nome {nome}, tem {nome.Length} caracteres.");
+
+Console.WriteLine("");
+//ToUpper
+Console.WriteLine("==== ToUpper ====");
+var tipo = "fogo";
+
+Console.WriteLine($"Charizard é um pokémon do tipo: {tipo.ToUpper()}");
+
+Console.WriteLine("");
+//ToLower
+Console.WriteLine("==== ToLower ====");
+var tipoSecundario = "VOADOR";
+
+Console.WriteLine($"Charizard é um pokémon do tipo: {tipoSecundario.ToLower()}");
+
+Console.WriteLine("");
+//Remove
+Console.WriteLine("==== Remove ====");
+Console.WriteLine($"Eu chamo meu {nome} de {nome.Remove(5)}.");
+
+Console.WriteLine("");
+// Replace
+Console.WriteLine("==== Replace ====");
 Console.WriteLine(observacao.Replace("Charizard", "Meu Pokémon"));
+
+Console.WriteLine("");
+// Split
+Console.WriteLine("==== Split ====");
+var pokemons = " Charmander, Charmeleon, Charizard";
+string[] listaDePokemons = pokemons.Split(',');
+
+foreach (var pokemon in listaDePokemons)
+    Console.WriteLine(pokemon);
+
+Console.WriteLine("");
+// Substring
+Console.WriteLine("==== Substring ====");
+Console.WriteLine(observacao.Substring(11,10));
+
+Console.WriteLine("");
+// IsNullOrEmpty
+Console.WriteLine("==== IsNullOrEmpty ====");
+string nomeDoPokemon = "";
+
+if (String.IsNullOrEmpty(nomeDoPokemon))
+    Console.WriteLine("O nome do Pokémon não pode ser nulo ou vazio");
+
+Console.WriteLine("");
