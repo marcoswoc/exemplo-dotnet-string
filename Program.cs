1f3c0e72681161eaa8540a88a869a194f9a783d0
Marcos Wellington <marcoswoc@gmail.com>
2022-01-22 21:51:29 -0300

    Adicionando parte do artigo

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// // Trim
+// var  observacao = " Charizard cospe fogo que é quente o suficiente para derreter pedregulhos. ";
+
+// Console.WriteLine($"Observação com espaço: {observacao}");
+// Console.WriteLine($"Observação sem espaço: {observacao.Trim()}");
+
+// // Length
+// var nome = "Charizard";
+
+// Console.WriteLine($"O nome {nome}, tem {nome.Length} caracteres.");
+
+// //ToUpper
+
+// var tipo = "fogo";
+
+// Console.WriteLine($"Charizard é um pokémon do tipo: {tipo.ToUpper()}");
+
+// //ToLower
+// var tipoSecundario = "VOADOR";
+
+// Console.WriteLine($"Charizard é um pokémon do tipo: {tipoSecundario.ToLower()}");
+
+// //Remove
+// Console.WriteLine($"Eu chamo meu {nome} de {nome.Remove(5)}.");
+
+// // Replace
+var  observacao = " Charizard cospe fogo que é quente o suficiente para derreter pedregulhos. ";
+
+Console.WriteLine(observacao.Replace("Charizard", "Meu Pokémon"));
